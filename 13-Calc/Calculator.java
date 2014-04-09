0fea018b3c17e80c644e828ad5bba6e8d2d6c22d
kaip629 <kaip629@gmail.com>
2014-04-08 21:04:07 -0400

    hw

--- 13-Calc/Calculator.java
+++ 13-Calc/Calculator.java
@@ -1,17 +1,48 @@
+import java.io.*;
+import java.util.*;
+//Im getting that my Integer.parseInt() function and subString() functions are not applicable, but I think that if this cooperated everything would be fine.
 public class Calculator{
-    public static int compute(String s){
-	Scanner n = new Scanner();
-	String input=n.;
-	int pos=0;
+    public static int compute(String str){
 	int prev=0;
+	int ans=0;
+	String input=str;
 	Stack nums = new Stack();
-	for(pos<input.length){
+	for(int pos=0;pos<input.length;pos++){
 	    if(input.charAt(pos)==' '){
-		input.subString(prev,pos);
-		prev=pos;
-	    }else{
-		nums.p
-	    }
+			String snum = input.subString(prev,pos);
+			nums.push(snum);
+			prev=pos;
+	    }else if(input.charAt(pos)=='+'){
+			int x=0;
+			while(nums.peek()!=null){
+				x=Integer.parseInt(nums.pop());
+				ans+=x;
+			}
+	    }else if(input.charAt(pos)=='-'){
+			int x=0;
+			while(nums.peek()!=null){
+				x=Integer.parseInt(nums.pop());
+				ans-=x;
+			}
+		}else if(input.charAt(pos)=='*'){
+			int x=0;
+			while(nums.peek()!=null){
+				x=Integer.parseInt(nums.pop());
+				ans=ans*x;
+			}
+		}else if(input.charAt(pos)=='/'){
+			int x=0;
+			while(nums.peek()!=null){
+				x=Integer.parseInt(nums.pop());
+				if(x==0){
+					System.out.println("divide by 0");
+				}else{
+					ans=ans/x;}
+			}
+		}	
 	}
     }
+	public static void main(String[]args){
+		compute("5 6 2 7 +");
+	}
 }
